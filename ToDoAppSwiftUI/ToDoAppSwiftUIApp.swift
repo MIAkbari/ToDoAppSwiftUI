3f655c799d3ccc15007553e95cb7c5cd0830df58
Mohammad <ma@bornak.de>
2022-02-04 21:28:32 +0330

    Initial Commit

---
+++ ToDoAppSwiftUI/ToDoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppSwiftUIApp.swift
+//  ToDoAppSwiftUI
+//
+//  Created by Mohammad on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
